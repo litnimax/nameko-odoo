7614c3746658a6bb9721b1e965173808fb93e25a
Maxim Litnitskiy <litnimaxster@gmail.com>
2019-10-15 20:49:00 +0300

    Refactoring

--- nameko_odoo/rpc_client.py
+++ nameko_odoo/rpc_client.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 import logging
 from nameko.extensions import DependencyProvider
 import odoorpc
@@ -31,29 +33,33 @@ class OdooClient(DependencyProvider):
         self.http_session = requests.Session()
 
     def setup_rpc_session(self):
-        try:
-            logger.info(
-                    'Connecting to Odoo at %s://%s:%s',
-                    self.odoo_protocol, self.odoo_host, self.odoo_port)
-            odoo = odoorpc.ODOO(self.odoo_host, port=self.odoo_port,
-                                protocol=self.odoo_protocol)
-            odoo.login(self.odoo_db, self.odoo_user, self.odoo_pass)
-            logger.info('Connected to Odoo as %s', self.odoo_user)
-            self.odoo = odoo
-            
-        except odoorpc.error.RPCError as e:
-            if 'res.users()' in str(e):
-                logger.error(
-                        'Odoo login %s not found or bad password %s, '
-                        'check in Odoo!',
-                        self.odoo_user, self.odoo_pass)
-            else:
-                logger.exception('RPC error:')
-        except URLError as e:
-            logger.error(e)
-        except Exception as e:
-            if 'Connection refused' in repr(e):
-                logger.error('Odoo refusing connection.')
-            else:
-                logger.exception(e)
+        while True:
+            try:
+                logger.info(
+                        'Connecting to Odoo at %s://%s:%s',
+                        self.odoo_protocol, self.odoo_host, self.odoo_port)
+                odoo = odoorpc.ODOO(self.odoo_host, port=self.odoo_port,
+                                    protocol=self.odoo_protocol)
+                odoo.login(self.odoo_db, self.odoo_user, self.odoo_pass)
+                logger.info('Connected to Odoo as %s', self.odoo_user)
+                self.odoo = odoo
+                break
+            except odoorpc.error.RPCError as e:
+                if 'res.users()' in str(e):
+                    logger.error(
+                            'Odoo login %s not found or bad password %s, '
+                            'check in Odoo!',
+                            self.odoo_user, self.odoo_pass)
+                else:
+                    logger.exception('RPC error:')
+            except URLError as e:
+                logger.error(e)
+            except Exception as e:
+                if 'Connection refused' in repr(e):
+                    logger.error('Odoo refusing connection.')
+                else:
+                    logger.exception(e)
+            logger.info('Sleeping a bit before next retry to setup Odoo session...')
+            eventlet.sleep(1)
+            # Continue
 
